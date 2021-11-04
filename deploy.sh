e4e4c40cfea47702380bc66178cf58e66b0f5b27
metasean <metasean@users.noreply.github.com>
2021-11-04 07:24:15 -0600

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -20,9 +20,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# git push -f git@github.com:metasean/Redroxmusic.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:Redroxmusic/2021.git master:gh-pages
 
 cd -
